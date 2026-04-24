358b9adf293a95b61989f2368c89ccd53ffdfa90
Jan Kudlicka <github@kudlicka.eu>
2026-04-24 16:08:37 +0200

    Add compile and run argument classes (#18)
    - Introduce classes for compile and run arguments.
    - Make compile arguments mirror tpplc arguments.
    - Support saving and loading run arguments.
    - Enable saving inference results to a file.
    - Remove obsolete syntax highlighting in Jupyter notebooks.
    - Refactor code for better PEP 8 compliance.

--- tests/test_all.py
+++ tests/test_all.py
@@ -5,23 +5,20 @@ import numpy as np
 
 def test_compilation_error():
     with pytest.raises(treeppl.CompileError):
-        with treeppl.Model(
-            source="""\
+        with treeppl.Model(source="""\
 model incorrect() {
   return result;
 }
-""",
-            samples=1,
-        ) as model:
+""") as model:
             model()
 
 
 def test_coin():
-    nsamples = 100
-    with treeppl.Model(filename="examples/coin.tppl", samples=nsamples) as coin:
+    particles = 100
+    with treeppl.Model(filename="examples/coin.tppl", m="smc-bpf", particles=particles) as coin:
         res = coin(outcomes=(np.random.random(10) < 0.5).tolist())
         assert isinstance(res, treeppl.InferenceResult)
-        assert len(res.samples) == nsamples
+        assert len(res.samples) == particles
 
 
 def test_matrix_mul():
@@ -35,7 +32,7 @@ model function matrix_mul(a: Matrix[Real], b: Matrix[Real]) => Matrix[Real] {
   return a *@ b;
 }
 """,
-        samples=1,
+        particles=1,
     ) as matrix_mul:
         result = matrix_mul(a=a, b=b).samples[0]
     assert (result == expected).all()
@@ -43,9 +40,7 @@ model function matrix_mul(a: Matrix[Real], b: Matrix[Real]) => Matrix[Real] {
 
 def test_tree_input():
     tree = treeppl.Tree.Node(
-        left=treeppl.Tree.Node(
-            left=treeppl.Tree.Leaf(age=0.0), right=treeppl.Tree.Leaf(age=0.0), age=0.5
-        ),
+        left=treeppl.Tree.Node(left=treeppl.Tree.Leaf(age=0.0), right=treeppl.Tree.Leaf(age=0.0), age=0.5),
         right=treeppl.Tree.Leaf(age=0.0),
         age=1.0,
     )
@@ -62,7 +57,7 @@ model function count_tree_leaves(tree: Tree) => Int {
   return count_leaves(tree);
 }
 """,
-        samples=1,
+        particles=1,
     ) as count_tree_leaves:
         res = count_tree_leaves(tree=tree)
         assert res.samples[0] == 3
--- treeppl/base.py
+++ treeppl/base.py
@@ -1,20 +1,20 @@
 import json
 from operator import itemgetter
 from tempfile import TemporaryDirectory
-from subprocess import Popen, PIPE, STDOUT
+import subprocess
 import numpy as np
-
 import tarfile
-import shutil
 import os
 import importlib
+import shlex
 
 from .exceptions import CompileError, InferenceError
 from .serialization import from_json, to_json
 
+
 def get_tpplc_binary():
-    if os.environ.get('TPPLC'):
-        return os.environ['TPPLC']
+    if os.environ.get("TPPLC"):
+        return os.environ["TPPLC"]
     # NOTE(vipa, 2025-06-04): The selfcontained compiler must be
     # deployed to a directory somewhere. There are three important
     # limitatations:
@@ -33,64 +33,105 @@ def get_tpplc_binary():
     #   might share (immutable) deployed directories between virtual
     #   envs, which seems fine, and that differing versions will get
     #   differing deployed directories.
-    tmp_dir = '/tmp'
-    deployed_basename = "@DEPLOYED_BASENAME@" # This will be substituted by nix when building the wheel
-    tarball_name = "@TARBALL_NAME@" # This will be substituted by nix when building the wheel
+    tmp_dir = "/tmp"
+    deployed_basename = "@DEPLOYED_BASENAME@"  # This will be substituted by nix when building the wheel
+    tarball_name = "@TARBALL_NAME@"  # This will be substituted by nix when building the wheel
     tppl_dir_path = os.path.join(tmp_dir, deployed_basename)
     if not os.path.isdir(tppl_dir_path):
-        with importlib.resources.path('treeppl', tarball_name) as tarball:
+        with importlib.resources.path("treeppl", tarball_name) as tarball:
             with tarfile.open(tarball) as tar:
-                if hasattr(tarfile, 'tar_filter'):
-                    tar.extractall(tmp_dir, filter='tar')
+                if hasattr(tarfile, "tar_filter"):
+                    tar.extractall(tmp_dir, filter="tar")
                 else:
                     # NOTE(vipa, 2025-11-20): This allows us to support Python < 3.12
                     tar.extractall(tmp_dir)
     return os.path.join(tppl_dir_path, "tpplc")
 
-class Model:
-    def __init__(
-        self,
-        source=None,
-        filename=None,
-        method="smc-bpf",
-        samples=1000,
-        subsamples=None,
-        **kwargs,
-    ):
-        self.temp_dir = TemporaryDirectory(prefix="treeppl_")
-        if filename:
-            source = open(filename).read()
-        if not source:
-            raise CompileError("No source code to compile.")
-        with open(self.temp_dir.name + "/__main__.tppl", "w") as f:
-            f.write(source)
-        args = [
-            get_tpplc_binary(),
-            "__main__.tppl",
-            "-m",
-            method,
-            "-p",
-            str(samples),
-        ]
-        if subsamples:
-            args.extend(["--subsample", "-n", str(subsamples)])
-        for k, v in kwargs.items():
-            args.append(f"--{k.replace('_', '-')}")
+
+class Arguments(dict):
+    def __getattr__(self, name):
+        return self[name]
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+
+class CompileArguments(Arguments):
+    def as_list(self):
+        res = []
+        for k, v in self.items():
+            k = k.replace("_", "-")
+            res.append(f"-{k}" if len(k) == 1 else f"--{k}")
             if v is not True:
-                args.append(str(v))
-        with Popen(
-            args=args, cwd=self.temp_dir.name, stdout=PIPE, stderr=STDOUT
-        ) as proc:
-            try:
-                proc.wait()
-            except KeyboardInterrupt:
-                output = proc.stdout.read().decode("utf-8")
-                output = output.replace("__main__.tppl", "source code")
-                raise CompileError(f"Could not compile the TreePPL model:\n{output}")
-            if proc.returncode != 0:
-                output = proc.stdout.read().decode("utf-8")
-                output = output.replace("__main__.tppl", "source code")
-                raise CompileError(f"Could not compile the TreePPL model:\n{output}")
+                res.append(str(v))
+        return res
+
+    def parse_opts(self, s):
+        t = shlex.split(s)
+        i = 0
+        while i < len(t):
+            k = t[i].lstrip("-").replace("-", "_")
+            if i + 1 < len(t) and not t[i + 1].startswith("-"):
+                v = t[i + 1]
+                i += 1
+                if v.isdigit():
+                    v = int(v)
+                else:
+                    try:
+                        v = float(v)
+                    except:
+                        pass
+            else:
+                v = True
+            self[k] = v
+            i += 1
+
+
+class RunArguments(Arguments):
+    def load_json(self, filename):
+        with open(filename) as f:
+            self.update(from_json(f))
+
+    @classmethod
+    def from_json(cls, filename):
+        with open(filename) as f:
+            data = from_json(f)
+        return cls(**data)
+
+    def save_json(self, filename):
+        with open(filename, "w") as f:
+            to_json(self, f)
+
+
+class Model:
+    def __init__(self, source=None, filename=None, **kwargs):
+        self.compile_arguments = None
+        self.run_arguments = None
+        self.source = source
+        if filename:
+            with open(filename) as f:
+                self.source = f.read()
+        self.temp_dir = TemporaryDirectory(prefix="treeppl_")
+        self.compile(**kwargs)
+
+    def compile(self, **kwargs):
+        self.compile_arguments = CompileArguments(kwargs)
+        if not self.source:
+            raise CompileError("no source code to compile")
+        with open(self.temp_dir.name + "/__main__.tppl", "w") as f:
+            f.write(self.source)
+        args = [get_tpplc_binary(), "__main__.tppl"]
+        args.extend(self.compile_arguments.as_list())
+        result = None
+        try:
+            result = subprocess.run(args=args, cwd=self.temp_dir.name, capture_output=True, text=True)
+        except KeyboardInterrupt:
+            pass
+        if result is None:
+            raise CompileError("could not compile the TreePPL model")
+        elif result.returncode != 0:
+            output = result.stdout.replace("__main__.tppl", "source code")
+            raise CompileError(f"could not compile the TreePPL model:\n{output}")
 
     def __enter__(self):
         return self
@@ -98,27 +139,35 @@ class Model:
     def __exit__(self, *args):
         self.temp_dir.cleanup()
 
-    def __call__(self, **kwargs):
-        with open(self.temp_dir.name + "/input.json", "w") as f:
-            to_json(kwargs or {}, f)
+    def run(self, arguments=None, **kwargs):
+        self.run_arguments = RunArguments(arguments or {}, **kwargs)
+        self.run_arguments.save_json(self.temp_dir.name + "/input.json")
         args = [
             f"{self.temp_dir.name}/out",
             f"{self.temp_dir.name}/input.json",
         ]
-        with Popen(args=args, stdout=PIPE) as proc:
+        with subprocess.Popen(args=args, stdout=subprocess.PIPE) as proc:
             return InferenceResult(proc.stdout)
+
+    def __call__(self, *args, **kwargs):
+        return self.run(*args, **kwargs)
 
 
 class InferenceResult:
     def __init__(self, stdout):
+        self.result = None
         try:
-            result = from_json(stdout)
+            self.result = from_json(stdout)
         except json.decoder.JSONDecodeError:
-            raise InferenceError("Could not parse the output from TreePPL.")
-        self.samples = result.get("samples", [])
-        self.weights = np.array(result.get("weights", []))
+            raise InferenceError("could not parse the output from TreePPL")
+        self.samples = self.result.get("samples", [])
+        self.weights = np.array(self.result.get("weights", []))
         self.nweights = np.exp(self.weights)
-        self.norm_const = result.get("normConst", np.nan)
+        self.norm_const = self.result.get("normConst", np.nan)
 
     def items(self, *args):
         return list(map(itemgetter(*args), self.samples))
+
+    def save_json(self, filename):
+        with open(filename, "w") as f:
+            to_json(self.result, f)
